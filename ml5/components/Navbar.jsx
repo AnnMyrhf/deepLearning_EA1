ef27cf8f3e6ef0911e023552dd7baa9eee6707bd
AnnMyrhf <ann-kathrin.meyerhof@stud.th-luebeck.de>
2026-03-25 14:31:40 +0100

    Seitenlayout angelegt

---
+++ ml5/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-secondary">
+            <div className="container">
+                <NavLink className="navbar-brand fw-bold" to="/">Start</NavLink>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Bilderkennung</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/diskussion">Diskussion</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/dokumentation">Dokumentation</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
