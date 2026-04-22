9a40e7cc5eade5effbe78e431099fc4f18c31800
AnnMyrhf <ann-kathrin.meyerhof@stud.th-luebeck.de>
2026-04-22 14:53:58 +0200

    Sticky Footer entfernt

--- ml5/components/Footer.jsx
+++ ml5/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer className="footer mt-auto py-3 bg-dark text-light border-top border-secondary fixed-bottom">
+        <footer className="footer mt-auto py-3 bg-dark text-light border-top border-secondary">
             <div className="container text-center">
         <span className="text-secondary small">
           © {currentYear} Ann-Kathrin Meyerhof
